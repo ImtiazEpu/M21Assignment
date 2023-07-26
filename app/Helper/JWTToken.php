d25787bd4628fc1342281499d17bda30d6e14858
Imtiaz Ahmed <info@imtiazepu.com>
2023-07-26 19:31:22 +0600

    code updated

--- app/Helper/JWTToken.php
+++ app/Helper/JWTToken.php
@@ -27,47 +27,4 @@ class JWTToken {
 
         return JWT::encode( $payload, $key, 'HS256' );
     }
-
-
-    /**
-     * Create token for set password
-     *
-     * @param $userEmail
-     *
-     * @return string
-     */
-    public static function CreateTokenForSetPassword( $userEmail ): string {
-        $key     = env( 'JWT_KEY' );
-        $payload = [
-            'iss'       => 'laravel-token',
-            'iat'       => time(),
-            'exp'       => time() + 60 * 20,
-            'userEmail' => $userEmail,
-            'userID'    => '0'
-        ];
-
-        return JWT::encode( $payload, $key, 'HS256' );
-    }
-
-
-    /**
-     * Create token for verify email
-     *
-     * @param $token
-     *
-     * @return object|string
-     */
-    public static function VerifyToken( $token ): object|string {
-        try {
-            if ( $token == null ) {
-                return 'unauthorized';
-            } else {
-                $key = env( 'JWT_KEY' );
-
-                return JWT::decode( $token, new Key( $key, 'HS256' ) );
-            }
-        } catch ( Exception $e ) {
-            return 'unauthorized';
-        }
-    }
 }
